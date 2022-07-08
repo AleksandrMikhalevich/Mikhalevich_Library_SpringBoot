f6ea29e1899b81c7a540da48b640c4e61b3909c0
AleksandrMikhalevich <aleksandr.mikhalevich89@gmail.com>
2022-07-08 11:50:53 +0300

    Initial commit

---
+++ src/main/java/by/it_academy/mikhalevich_library_springboot/MikhalevichLibrarySpringBootApplication.java
@@ -0,0 +1,13 @@
+package by.it_academy.mikhalevich_library_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikhalevichLibrarySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MikhalevichLibrarySpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/it_academy/mikhalevich_library_springboot/MikhalevichLibrarySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package by.it_academy.mikhalevich_library_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikhalevichLibrarySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
