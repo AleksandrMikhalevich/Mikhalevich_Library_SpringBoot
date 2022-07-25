0015517afc93a7962c35409e57692e2f9f4c9d64
AleksandrMikhalevich <aleksandr.mikhalevich89@gmail.com>
2022-07-25 13:41:58 +0300

    Constants added

---
+++ src/main/java/by/it_academy/mikhalevich_library_springboot/constants/Constants.java
@@ -0,0 +1,61 @@
+package by.it_academy.mikhalevich_library_springboot.constants;
+
+/**
+ * @author Alex Mikhalevich
+ * @created 2022-07-25 12:46
+ */
+public class Constants {
+    public static final String CURRENT_PAGE = "currentPage";
+    public static final String PAGE_SIZE = "pageSize";
+    public static final String SORT_FIELD = "sortField";
+    public static final String SORT_DIR = "sortDir";
+    public static final String TOTAL_PAGES = "totalPages";
+    public static final String TOTAL_ITEMS = "totalItems";
+    public static final String REVERSE_SORT_DIR = "reverseSortDir";
+    public static final String SESSION_PAGE = "page";
+    public static final String SESSION_SIZE = "size";
+    public static final String SESSION_FIELD = "field";
+    public static final String SESSION_DIR = "dir";
+    public static final String SESSION_BOOK = "book";
+    public static final String SESSION_AUTHOR = "author";
+    public static final String BOOK_AUTHORS = "bookAuthors";
+    public static final String BOOK_GENRES = "bookGenres";
+    public static final String BOOK_PUBLISHER = "bookPublisher";
+    public static final String BOOK_TITLE_FILTER = "titleFilter";
+    public static final String BOOK_LANGUAGE_FILTER = "languageFilter";
+    public static final String BOOK_SUMMARY_FILTER = "summaryFilter";
+    public static final String BOOK_AUTHOR_FILTER = "authorFilter";
+    public static final String BOOK_GENRE_FILTER = "genreFilter";
+    public static final String BOOK_PUBLISHER_FILTER = "publisherFilter";
+    public static final String BOOK_FROM_DATE_FILTER = "fromDateFilter";
+    public static final String BOOK_TO_DATE_FILTER = "toDateFilter";
+    public static final String BOOK_FROM_YEAR_FILTER = "fromYearFilter";
+    public static final String BOOK_TO_YEAR_FILTER = "toYearFilter";
+    public static final String BOOKS = "books";
+    public static final String AUTHORS = "authors";
+    public static final String AUTHOR_FIRST_NAME_FILTER = "firstNameFilter";
+    public static final String AUTHOR_SECOND_NAME_FILTER = "secondNameFilter";
+    public static final String AUTHOR_SURNAME_FILTER = "surnameFilter";
+    public static final String AUTHOR_COUNTRY_FILTER = "countryFilter";
+    public static final String AUTHOR_BOOK_FILTER = "bookFilter";
+    public static final String AUTHOR_PUBLISHER_FILTER = "publisherFilter";
+    public static final String GENRES = "genres";
+    public static final String GENRE_NAME_FILTER = "nameFilter";
+    public static final String GENRE_DESCRIPTION_FILTER = "descriptionFilter";
+    public static final String GENRE_BOOK_FILTER = "bookFilter";
+    public static final String PUBLISHERS = "publishers";
+    public static final String PUBLISHER_NAME_FILTER = "nameFilter";
+    public static final String PUBLISHER_COUNTRY_FILTER = "countryFilter";
+    public static final String PUBLISHER_CITY_FILTER = "cityFilter";
+    public static final String PUBLISHER_STREET_FILTER = "streetFilter";
+    public static final String PUBLISHER_HOUSE_FILTER = "houseFilter";
+    public static final String PUBLISHER_ZIPCODE_FILTER = "zipcodeFilter";
+    public static final String AUTHOR_PUBLISHERS = "authorPublishers";
+    public static final String GENRE = "genre";
+    public static final String PUBLISHER = "publisher";
+    public static final int DEFAULT_PAGE_NUMBER_1 = 1;
+    public static final int DEFAULT_PAGE_SIZE_5 = 5;
+    public static final String DEFAULT_SORT_FIELD_ID = "id";
+    public static final String DEFAULT_SORT_DIR_ASC = "asc";
+    public static final String SORT_DIR_DESC = "desc";
+}
