88fdf170f0c71c7e500f61ac6cb33b31d82192ee
AleksandrMikhalevich <aleksandr.mikhalevich89@gmail.com>
2022-08-01 12:29:30 +0300

    User repository updated

--- src/main/java/by/it_academy/mikhalevich_library_springboot/repositories/UserRepository.java
+++ src/main/java/by/it_academy/mikhalevich_library_springboot/repositories/UserRepository.java
@@ -2,8 +2,9 @@ package by.it_academy.mikhalevich_library_springboot.repositories;
 
 import by.it_academy.mikhalevich_library_springboot.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
 
     User findByLogin(String login);
 }
